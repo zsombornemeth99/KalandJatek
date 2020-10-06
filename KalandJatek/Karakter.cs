85c50db3ca3f4faa5dc692d7f1d9dd202a4e00ac
Petrik Lajos Szg <info@petrik.hu>
2020-10-06 10:57:40 +0200

    getterek, setterek megadása

--- KalandJatek/Karakter.cs
+++ KalandJatek/Karakter.cs
@@ -1,17 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KalandJatek
+﻿namespace KalandJatek
 {
     class Karakter
     {
         string nev;
         int eletEro;
         int sebzes;
-        string[,] kepessegek = new string [] ;
 
+
+
+        public Karakter(string nev, int milyenKepesseg)
+        {
+            this.nev = nev;
+            this.eletEro = 100;
+
+
+            if (milyenKepesseg == 0)
+                this.Sebzes = 10;
+            else if (milyenKepesseg == 1)
+                this.Sebzes = 8;
+            else
+                this.Sebzes = 11;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int EletEro { get => eletEro; set => eletEro = value; }
+        public int Sebzes { get => sebzes; set => sebzes = value; }
     }
 }
